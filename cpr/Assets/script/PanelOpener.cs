68db17241b4eda08194be253c5e03d3c76e27c95
22611077a <114436235+22611077a@users.noreply.github.com>
2023-07-05 22:04:14 +0800

    Scene2

---
+++ cpr/Assets/script/PanelOpener.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelOpener : MonoBehaviour
+{
+    public GameObject Panel;
+    
+    public void OpenPanel()
+    {
+        if(Panel != null)
+        {
+            Panel.SetActive(true);
+        }
+    }
+}
